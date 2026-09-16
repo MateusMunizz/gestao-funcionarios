005240d8d4a0e25ffe4d8aaeb93c310caaa47812
MateusMunizz <mateusmuniz1090@hotmail.com>
2026-09-16 13:00:47 -0300

    feat: estrutura inicial do projeto

---
+++ src/controllers/funcionarioController.js
@@ -0,0 +1,19 @@
+import db from '../config/db.js';
+
+export const listarFuncionarios = (req, res) => {
+  db.query('SELECT * FROM funcionarios', (err, results) => {
+    if(err) return res.status(500).json( { error: 'Erro ao buscar funcionários' });
+    res.json(results);
+  });
+}
+
+export const inserirFuncionario = (req, res) => {
+
+  const {nome, cargo, salario} = req.body;
+
+  const sql = 'INSERT INTO funcionarios (nome, cargo, salario) VALUES (?, ?, ?)';
+  db.query(sql, [nome, cargo, salario], err => {
+    if(err) return res.status(500).json( { error: 'Erro ao inserir funcionário' });
+    res.json({ mensagem: 'Funcionário inserido com sucesso' });
+  });
+};
